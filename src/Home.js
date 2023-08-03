631d4a6e14b4055cd37ad30705ad001660787ff2
Jaimin78 <jaiminsuthar7802@gmail.com>
2023-08-03 15:20:43 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Navigation from "../src/Navigation";
+import './App.css'
 const Home = () => {
     return (
         <>
             <Navigation />
             <div className="w-100">
-                <h1>Hello</h1>
+                <div className="container">
+                    <div className="title">Menu</div>
+                    
+                    <div className="food-group">
+                        <div className="food-card">
+                            <div className="food-img">
+                          
+                            </div>
+                            <div  className="food-details">
+                                item name, veg/nonveg
+                                category,
+                                description,
+                                price, add to carts
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
